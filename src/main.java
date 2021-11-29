d2580d4c16c0057a75fc5d674477aefc27dbf846
vicore6 <vicore6@gmail.com>
2021-11-29 17:50:12 +0300

    oks

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
-public class main {
-    public static void main (String[] args) {
+public class Main {
+    public static void Main (String[] args) {
         int balance = 2_000_000_000;
         int transfer = 500_000_000;
         int bank = balance + transfer;
